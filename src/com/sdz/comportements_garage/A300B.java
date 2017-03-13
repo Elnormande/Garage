b1e467b9480d88533b7c10bbd8b20d16c629d57a
alibert <alibert@NANTES-0103.sii.fr>
2017-03-13 10:38:00 +0100

    Final avec quelques corrections

--- src/com/sdz/comportements_garage/A300B.java
+++ src/com/sdz/comportements_garage/A300B.java
@@ -2,8 +2,6 @@ package com.sdz.comportements_garage;
 
 public class A300B extends Vehicule{
 
-	private static final long serialVersionUID = 8350597177647919876L;
-
 	public A300B() {
 		super(45000, "A300B", Marque.RENO);
 	}
